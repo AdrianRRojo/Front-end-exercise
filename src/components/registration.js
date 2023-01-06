44aee398fe829426b70fb69b119851df480eabc6
Adrian Rojo <rojoadrian16@gmail.com>
2023-01-06 11:47:19 -0600

    created drop down menus for states and occupations

--- src/components/registration.js
+++ src/components/registration.js
@@ -7,6 +7,7 @@ export default function Registration(){
     return(
         <div>
             <div>
+                <form>
                 <div>
                   <label for="fullName">Full Name: </label>
                   <input type="text" id="fullName" placeholder="Full Name" required/>
@@ -19,19 +20,78 @@ export default function Registration(){
                   <label for="password">Password: </label>
                   <input type="text" id="password" placeholder="Password" required/>
                 </div>
-                {/* <div>
-                  <label for="Occupation">Occupation</label>
-                  <input type="text" id="Occupation" placeholder="Occupation" required/>
-                </div> */}
+                <div name='occupation' id='occupation'>
+                    <label for='occupation'>Occupation: </label>
+                    <select>
+                        <option value= ''> Select an occupation </option>
+                        <option value='frontend'>Front End Developer</option>
+                        <option value='backend'>Back End Developer</option>
+                        <option value='fullstack'>Full Stack Developer</option>
+                    </select>
+                </div>
                   <div>
-                  <label for="state">State: </label>
-                  <input type="text" id="state" placeholder="State" required/>
+                  <label for="states">State: </label>
+                  <select>
+                  <option value="AL">Alabama</option>
+	                <option value="AK">Alaska</option>
+	                <option value="AZ">Arizona</option>
+	                <option value="AR">Arkansas</option>
+	                <option value="CA">California</option>
+	                <option value="CO">Colorado</option>
+	                <option value="CT">Connecticut</option>
+	                <option value="DE">Delaware</option>
+	                <option value="DC">District Of Columbia</option>
+	                <option value="FL">Florida</option>
+	                <option value="GA">Georgia</option>
+	                <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY">New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
+                  </select>
                 </div>
                 <div>
                     <br/>
                     <button className=" bg-orange-400 hover:bg-orange-500 rounded-full btn " type='submit'>Submit</button>
                 </div>
+                </form>
             </div>
+
         </div>
     )
 }
