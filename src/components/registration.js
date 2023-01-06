38bd836534cb4aa70414c1f29e00c09f3deb3875
Adrian Rojo <rojoadrian16@gmail.com>
2023-01-06 13:52:27 -0600

    set user info states

--- src/components/registration.js
+++ src/components/registration.js
@@ -6,7 +6,8 @@ import "./style.css"
 
 export default function Registration(){
     // User Information
-    const [fullName, setFullName] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [occupation, setOccupation] = useState('')
@@ -31,48 +32,101 @@ export default function Registration(){
     // console.log('Occupation ->',occupationOptions[0])
     // console.log('states ->',stateOptions[0])
 
-    
+    const handleInputChange = (e) => {
+        const {id , value} = e.target;
+        if(id === "firstName"){
+            setFirstName(value);
+        }
+        if(id === "lastName"){
+            setLastName(value);
+        }
+        if(id === "email"){
+            setEmail(value);
+        }
+        if(id === "password"){
+            setPassword(value);
+        }
+        if(id === "occupation"){
+            setOccupation(value);
+        }
+        if(id === "state"){
+            setState(value);
+        }
+
+
+    }
+
+    const handleSubmit  = () => {
+        console.log(
+            "first -> ", firstName,
+            "last -> ", lastName,
+            "email -> ", email,
+            "password -> ", password,
+            "occupation -> ",occupation, 
+            "state -> ", state
+            );
+    }
     return(
         <div>
             <div>
                 {/* User info */}
                 <form>
                 <div>
-                  <label htmlFor="fullName">Full Name: </label>
-                  <input type="text" id="fullName" placeholder="Full Name" required/>
-                  
-                </div>
-                <div>
-                  <label htmlFor="email">Email: </label>
-                  <input type="text" id="email" placeholder="Email" required/>
-                </div>
-                <div>
-                  <label htmlFor="password">Password: </label>
-                  <input type="text" id="password" placeholder="Password" required/>
+                  <label htmlFor="firstName">First Name: </label>
+                  <input 
+                    type="text" id="firstName" placeholder="First Name" 
+                    value={firstName} onChange = {(e) => handleInputChange(e)} required/>
                 </div>
 
                 <div>
-                    {/* Using data retrieved from line 19 useEffect to create an option for every occupation and states */}
-                <label htmlFor="occupation">Occupation: </label>
-                    <select id="occupation"> 
-                        {occupationOptions[0].map((occupation, i) => (
-                            <option id="occupation" key={i}>{occupation}</option>
-                        ))}
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="states">State: </label>
-                    <select id="states">
-                        {stateOptions[0].map((state, i) => (
-                            <option id="states" key={i}>{`${state.name}`}</option>
-                        ))}
-                    </select>
+                  <label htmlFor="lastName">last Name: </label>
+                  <input 
+                    type="text" id="lastName" placeholder="Last Name"
+                    value={lastName} onChange = {(e) => handleInputChange(e)}required/>
                 </div>
                 
                 <div>
-                    <br/>
-                    <button className=" bg-orange-400 hover:bg-orange-500 rounded-full btn " type='submit'>Submit</button>
+                  <label htmlFor="email">Email: </label>
+                  <input 
+                    type="text" id="email" 
+                    placeholder="Email" value={email} onChange = {(e) => handleInputChange(e)} required/>
                 </div>
+
+                <div>
+                
+                  <label htmlFor="password">Password: </label>
+                  <input 
+                    type="text" id="password" placeholder="Password" 
+                    value={password} onChange = {(e) => handleInputChange(e)} required/>
+                </div>
+
+                <div>
+            {/* Using data retrieved from line 19 useEffect to create an option for every occupation and states */}
+                <label htmlFor="occupation">Occupation: </label>
+                    <select id="occupation"> 
+                        {occupationOptions[0].map((occupation, i) => (
+                            <option 
+                                id="occupation" key={i} 
+                                value={occupation} onChange = {(e) => handleInputChange(e)} required>{occupation}</option>
+                        ))}
+                    </select>
+                </div>
+
+                <div>
+                <label htmlFor="states">State: </label>
+                    <select id="states">
+                        {stateOptions[0].map((state, i) => (
+                            <option 
+                                id="states" key={i} 
+                                value={state} onChange = {(e) => handleInputChange(e)} required>{`${state.name}`}</option>
+                        ))}
+                    </select>
+                </div>
+
+                <div>
+                    <br/>
+                    <button onClick={()=>handleSubmit()} className=" bg-orange-400 hover:bg-orange-500 rounded-full btn " type='submit'>Submit</button>
+                    </div>
                 </form>
             </div>
 
